383a30d94650a385b2ce9d28feb227d44a838afb
Nguyenletrungthien <101155715+Nguyenletrungthien@users.noreply.github.com>
2022-10-18 18:48:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng chị của em';
+const contentCard = 'Hôm nay là một ngày thật đặc biệt. Em muốn gửi tới chị lời chúc tốt lành và may mắn nhất. Chúc chị luôn rạng ngời, xinh đẹp và luôn thành công trong công việc cũng như cuộc sống.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
